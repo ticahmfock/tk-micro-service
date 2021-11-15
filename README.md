8a6eb1e9612ce0eac308d00b3b69f435afcbff14
ticahmfock <294962719@qq.com>
2021-11-15 16:21:53 +0800

    初始化代码

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 #微服务
+## 1、版本信息
+### SpringBoot[2.3.0.RELEASE]
+### SpringCloud[Hoxton.SR8]
+## 2、注册中心(eureka)
+
--- gateway-service/src/main/java/com/tk/Gateway.java
+++ gateway-service/src/main/java/com/tk/Gateway.java
@@ -2,17 +2,17 @@ package com.tk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author: TK
- * @date: 2021/11/15 15:07
+ * @date: 2021/11/15 15:35
  */
-@EnableEurekaServer
+@EnableEurekaClient
 @SpringBootApplication
-public class Eureka {
+public class Gateway {
 
   public static void main(String[] args) {
-    SpringApplication.run(Eureka.class, args);
+    SpringApplication.run(Gateway.class, args);
   }
 }
